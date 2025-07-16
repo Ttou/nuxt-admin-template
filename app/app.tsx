90f6f04b634eea0002567983e1016e9595e0af92
wangruijin <1361572192@qq.com>
2025-07-16 21:58:52 +0800

    🐞 fix: 修复类型问题

--- app/app.tsx
+++ app/app.tsx
@@ -1,3 +1,7 @@
+// FIX types
+import '../.nuxt/types/imports.d.ts'
+import '../node_modules/pinia-plugin-persistedstate/dist/index.d.ts'
+
 import { NuxtLayout, NuxtPage } from '#components'
 import { ElConfigProvider } from 'element-plus'
 
